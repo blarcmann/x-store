5b034b6df19690f4b76c91f9d1ee07d966b8042d
Odetola Azeez Opeyemi <yemiotola@gmail.com>
2022-09-22 14:14:15 +0100

    updated create product fields

--- api/models/Product.js
+++ api/models/Product.js
@@ -4,13 +4,14 @@ const ProductSchema = new mongoose.Schema(
   {
     title: { type: String, required: true, unique: true },
     description: { type: String, required: true },
-    img: { type: Array, required: true },
+    images: { type: Array, required: true },
+    sku: { type: String, required: true },
     price: { type: Number, required: true },
-    categories: { type: Array, required: true },
-    discount: { type: Number },
-    size: { type: Array },
-    color: { type: Array },
+    salePrice: { type: Number },
+    tags: { type: Array, required: true },
     inStock: { type: Boolean, default: true },
+    gender: { type: String, required: true },
+    category: { type: String, required: true },
   },
   { timestamps: true }
 );
